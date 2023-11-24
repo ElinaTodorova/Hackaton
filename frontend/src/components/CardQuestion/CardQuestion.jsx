c50fbe9443d54191c86ce5396a23cba58881bb93
AXEL LARESE <axelarese@gmail.com>
2023-11-24 09:45:21 +0100

    stufffffs

--- frontend/src/components/CardQuestion/CardQuestion.jsx
+++ frontend/src/components/CardQuestion/CardQuestion.jsx
@@ -2,7 +2,6 @@ import { useParams } from "react-router-dom";
 import Data from "../../../Data.json";
 import DataBis from "../../../DataBis.json";
 import styles from "./CardQuestion.module.css";
-import Header from "../Header";
 import { useTheme } from "../contexts/ThemeContext";
 
 export default function CardQuestion() {
@@ -16,24 +15,31 @@ export default function CardQuestion() {
   }
 
   return (
-    <>
-      <Header />
-      <div className={styles.questionContainer}>
-        {dataTableau
-          .filter((element) => Number(element.id) === Number(id))
-          .map((question) => (
-            <div className={styles.mainContainerCard} key={question.id}>
-              <div className={styles.imageContainer}>
-                <img src={question.image} alt="some" />
-              </div>
+    <div
+      className={
+        isLight
+          ? `${styles.positionContainer} ${styles.lightMode}`
+          : `${styles.positionContainer} ${styles.darkMode}`
+      }
+    >
+      <div className={styles.cardHover}>
+        <div className={styles.questionContainer}>
+          {dataTableau
+            .filter((element) => Number(element.id) === Number(id))
+            .map((question) => (
+              <div className={styles.mainContainerCard} key={question.id}>
+                <div className={styles.imageContainer}>
+                  <img src={question.image} alt="some" />
+                </div>
 
-              <p className={styles.question}> {question.question}</p>
-              <div>
-                <p className={styles.answer}>{question.reponse}</p>
+                <p className={styles.question}> {question.question}</p>
+                <div>
+                  <p className={styles.answer}>{question.reponse}</p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+        </div>
       </div>
-    </>
+    </div>
   );
 }
