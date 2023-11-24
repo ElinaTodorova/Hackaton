de5a4edede5b626fbab84fbd10ebf689a1a1b821
AXEL LARESE <axelarese@gmail.com>
2023-11-24 08:56:56 +0100

    Icone flamme

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,13 +8,7 @@ export default function Header() {
     setIsLight(!isLight);
   };
   return (
-    <div
-      className={
-        isLight
-          ? `${styles.navBar} ${styles.lightMode}`
-          : `${styles.navBar} ${styles.darkMode}`
-      }
-    >
+    <div className={isLight ? `${styles.lightMode}` : `${styles.darkMode}`}>
       <div className={styles.header}>
         <img src="./images/logo.png" alt="LOGO Santa's Elves" />
       </div>
@@ -35,9 +29,9 @@ export default function Header() {
             className={styles.svgNavBar}
             xmlns="http://www.w3.org/2000/svg"
             height="1em"
-            viewBox="0 0 384 512"
+            viewBox="0 0 448 512"
           >
-            <path d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z" />
+            <path d="M159.3 5.4c7.8-7.3 19.9-7.2 27.7 .1c27.6 25.9 53.5 53.8 77.7 84c11-14.4 23.5-30.1 37-42.9c7.9-7.4 20.1-7.4 28 .1c34.6 33 63.9 76.6 84.5 118c20.3 40.8 33.8 82.5 33.8 111.9C448 404.2 348.2 512 224 512C98.4 512 0 404.1 0 276.5c0-38.4 17.8-85.3 45.4-131.7C73.3 97.7 112.7 48.6 159.3 5.4zM225.7 416c25.3 0 47.7-7 68.8-21c42.1-29.4 53.4-88.2 28.1-134.4c-4.5-9-16-9.6-22.5-2l-25.2 29.3c-6.6 7.6-18.5 7.4-24.7-.5c-16.5-21-46-58.5-62.8-79.8c-6.3-8-18.3-8.1-24.7-.1c-33.8 42.5-50.8 69.3-50.8 99.4C112 375.4 162.6 416 225.7 416z" />
           </svg>
         )}
       </div>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // eslint-disable-next-line import/no-extraneous-dependencies
-import Snowfall from "react-snowfall";
+// import Snowfall from "react-snowfall";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
@@ -25,10 +25,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Snowfall />
+    {/* <Snowfall /> */}
     <ThemeProvider>
       <WrapComponent>
-        <Snowfall />
+        {/* <Snowfall /> */}
         <RouterProvider router={router} />
       </WrapComponent>
     </ThemeProvider>
