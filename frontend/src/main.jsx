a541840dd69bf8ddc2e56130696813c422b7b2f6
AXEL LARESE <axelarese@gmail.com>
2023-11-24 11:14:07 +0100

    stufss

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// eslint-disable-next-line import/no-extraneous-dependencies
-// import Snowfall from "react-snowfall";
+import Snowfall from "react-snowfall";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
@@ -25,10 +24,16 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    {/* <Snowfall /> */}
     <ThemeProvider>
       <WrapComponent>
-        {/* <Snowfall /> */}
+        <Snowfall
+          style={{
+            position: "fixed",
+            width: "100vw",
+            height: "100vh",
+          }}
+          snowflakeCount={600}
+        />
         <RouterProvider router={router} />
       </WrapComponent>
     </ThemeProvider>
